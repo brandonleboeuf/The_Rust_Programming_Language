6309f0bfe5233bcaef15515911495c3422883b4f
Albert Wang <brandon.leboeuf@gmail.com>
2024-02-24 21:22:24 -0800

    Commit after running 'cargo run'

---
+++ hello_cargo/target/debug/deps/hello_cargo-5626a597dc58fa56.d
@@ -0,0 +1,5 @@
+/Users/bleb/DEV/learning/RUST/The_Rust_Programming_Language/hello_cargo/target/debug/deps/hello_cargo-5626a597dc58fa56: src/main.rs
+
+/Users/bleb/DEV/learning/RUST/The_Rust_Programming_Language/hello_cargo/target/debug/deps/hello_cargo-5626a597dc58fa56.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/bleb/DEV/learning/RUST/The_Rust_Programming_Language/hello_cargo/target/debug/hello_cargo: /Users/bleb/DEV/learning/RUST/The_Rust_Programming_Language/hello_cargo/src/main.rs
