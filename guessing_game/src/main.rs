dd770091bcedc1538373551fe3a10fd19e38258c
Albert Wang <brandon.leboeuf@gmail.com>
2024-02-27 20:17:57 -0800

    compleetes game with exit statement

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,20 +1,43 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
-use rand:: Rng;
 
 fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("The secret number is :{secret_number}");
+    println!("{secret_number}");
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    println!("You guessed: {guess}");
+        // Check if the user wants to crash the program by typing any letter
+        if guess.trim().chars().count() == 1 {
+            panic!("Intentionally crashing the program!");
+        }
+
+        // Continue with the number guessing logic
+        let guess: i32 = match guess.trim().parse::<i32>() {
+            Ok(num) => num + 2,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
